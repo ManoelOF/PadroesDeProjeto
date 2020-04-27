8bbc1e7c948e6c0289eaea16a60960ccbdb45dc6
ManoelOF <manoelaugusto2019@gmail.com>
2020-04-26 21:56:04 -0300

    adicionando instancia da classe dentro da classe principal main

--- src/Principal/Principal.java
+++ src/Principal/Principal.java
@@ -1,9 +1,11 @@
 package Principal;
 
+import java.sql.Connection;
+
 public class Principal {
 
 	public static void main(String[] args) {
-		
+		Connection con = ConnectionFactory.getConnection();
 
 	}
 
